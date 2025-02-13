60b9370f8d84317b96b100adefaba7c2f8da2ffd
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-13 11:32:11 -0600

    understanding the Option Enum and the None Option Enum.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let a: Option<i32> = Option::Some(5);
+    let b: Option<&str> = Option::Some("Taco");
+    let c: Option<bool> = Option::Some(true);
+
+    let a = Option::<i8>::Some(5);
+
+    let d: Option<&str> = Option::None;
+}
