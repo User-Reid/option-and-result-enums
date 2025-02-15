187163e174531f2550bc621e04a3070672db8226
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-14 23:33:23 -0600

    understanding the unwrap method and the unwrap_or method fully by creating them from scratch and implimenting them. :)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-fn main() {
-    let present_value: Option<i32> = Some(13);
-    let missing_value: Option<bool> = None;
+#[derive(Debug, Clone, Copy)]
+enum MyOption {
+    Some(i32),
+    None,
+}
 
-    println!("{}", present_value.unwrap_or(0));
-    println!("{}", missing_value.unwrap_or(false));
+impl MyOption {
+    fn unwrap(self) -> i32 {
+        match self {
+            MyOption::Some(value) => {
+                println!("{value}");
+                return value;
+            }
+            MyOption::None => panic!("YOU DONE FUCKED UP COUSIN"),
+        }
+    }
+
+    fn unwrap_or(self, fallback_value: i32) -> i32 {
+        match self {
+            MyOption::Some(value) => value,
+            MyOption::None => fallback_value,
+        }
+    }
+}
+
+fn main() {
+    let some_option: MyOption = MyOption::Some(12);
+    let none_option: MyOption = MyOption::None;
+
+    some_option.unwrap();
+    // none_option.unwrap();
+    println!("{}", none_option.unwrap_or(20));
 }
