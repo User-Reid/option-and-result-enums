157ba7b72c9326b96c1818020647e2819e32066c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-17 20:39:48 -0600

    understanding the use case and importance of returning a result enum from a function and locking down all possible outcomes with a match statement. :)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::num::ParseIntError;
+fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
+    if denominator == 0.0 {
+        Err(String::from("You fucked up cousin"))
+    } else {
+        Ok(numerator / denominator)
+    }
+}
 
 fn main() {
-    let text: &str = "30";
-    let text_as_number: Result<i8, ParseIntError> = text.parse::<i8>();
+    let result: Result<f64, String> = divide(2.0, 1.0);
 
-    println!("{:?}", text_as_number);
-
-    let text: &str = "Rocket";
-    let text_as_number: Result<i8, ParseIntError> = text.parse::<i8>();
-
-    println!("{:?}", text_as_number)
+    match result {
+        Ok(value) => println!("The result is {value}"),
+        Err(message) => println!("Error: {message}"),
+    }
 }
